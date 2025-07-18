1439e2d10a2aa9909836d440fbfe5e0e2e042f9c
nruharish <bs.harish@gmail.com>
2025-07-18 18:57:34 +0530

    second commit

--- Model.py
+++ Model.py
@@ -6,10 +6,15 @@ class Model(nn.Module):
     
     def __init__(self, num_features):
         super().__init__()
-        self.linear = nn.Linear(num_features, 1)
+        self.linear_layer1 = nn.Linear(num_features, 3)
+        self.relu = nn.ReLU()
+        self.linear_layer2 = nn.Linear(3, 1)
         self.sigmoid = nn.Sigmoid()
+
     
     def forward(self, features):
-        out = self.linear(features)
+        out = self.linear_layer1(features)
+        out = self.relu(out)
+        out = self.linear_layer2(out)
         out = self.sigmoid(out)
         return out
